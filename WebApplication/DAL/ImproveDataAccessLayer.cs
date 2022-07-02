7a1f963e4f5f22da048c5ebc989f3b3e5c20a839
ToanNguyen <toan@immexgroup.com>
2022-07-02 16:26:18 +0700

    Add improve

---
+++ WebApplication/DAL/ImproveDataAccessLayer.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using WebApplication.DAL.Models;
+
+namespace WebApplication.DAL
+{
+    public class ImproveDataAccessLayer
+    {
+        private readonly ShopContext _context;
+
+        public ImproveDataAccessLayer(ShopContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<CoffeeType> GetVietnameseCoffeeTypes()
+        {
+            return GetAllCoffeeTypes("Vietnam").ToList();
+        }
+
+        public decimal GetAmericanCoffeePrice()
+        {
+            var americanCoffees = GetAllCoffeeTypes("America").FirstOrDefault();
+            return americanCoffees == null ? 0 : americanCoffees.Price;
+        }
+
+        public IEnumerable<CoffeeType> SearchAustralianCoffeeTypes(User user)
+        {
+            var types = GetAllCoffeeTypes("Australia").ToList().AsReadOnly();
+
+            if (user != null)
+            {
+                var u = _context.UserCoffeePreferences.SingleOrDefault(preference => preference.UserId == user.UserId);
+                if (u != null)
+                {
+                    u.AustralianCoffeeSearch++;
+                    _context.SaveChanges();
+                }
+            }
+
+            return types;
+        }
+
+        public void IncreasePrices(IEnumerable<CoffeeType> coffeeTypes, double percentage)
+        {
+            if (coffeeTypes == null || coffeeTypes.Count() == 0 || percentage == 0)
+            {
+                return;
+            }
+
+            var ids = coffeeTypes.Select(p => p.CoffeeTypeId);
+            var l = _context.CoffeeTypes.Where(p => ids.Contains(p.CoffeeTypeId)).ToList();
+
+            if (l.Count == 0)
+            {
+                return;
+            }
+
+            // Update Price
+            foreach (var type in l)
+            {
+                type.Price *= 1 + (decimal)percentage;
+            }
+            _context.SaveChanges();
+        }
+
+        private IQueryable<CoffeeType> GetAllCoffeeTypes(string countryOfOrigin)
+        {
+            return _context.CoffeeTypes.Where(p => p.CountryOfOrigin == countryOfOrigin);
+        }
+    }
+}
