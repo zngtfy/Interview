60e564be89ab712f838cc625c583ac39945b138b
ToanNguyen <toan@immexgroup.com>
2022-07-02 14:13:49 +0700

    De bai

---
+++ Interview/CoffeeShop.cs
@@ -0,0 +1,59 @@
+﻿namespace Interview
+{
+    public class CoffeeShop
+    {
+        public double CalculateCoffeeCost(CoffeeOrder order)
+        {
+            double cost;
+            if (order.BeanType == BeanType.Vietnamese)
+            {
+                cost = 2.5;
+            }
+            else if (order.BeanType == BeanType.Jamaican)
+            {
+                cost = 2.3;
+            }
+            else
+            {
+                cost = 2.0;
+            }
+            if (order.IsDoubleShot)
+            {
+                cost *= 1.5;
+            }
+            if (order.HasMilk)
+            {
+                cost += 0.5;
+            }
+            if (order.Size == CoffeeSize.Medium)
+            {
+                cost += 1;
+            }
+            else if (order.Size == CoffeeSize.Large)
+            {
+                cost += 1.5;
+            }
+            return cost;
+        }
+    }
+
+    public class CoffeeOrder
+    {
+        public BeanType BeanType { get; set; }
+        public bool IsDoubleShot { get; set; }
+        public bool HasMilk { get; set; }
+        public CoffeeSize Size { get; set; }
+    }
+
+    public enum BeanType
+    {
+        Vietnamese,
+        Jamaican
+    }
+
+    public enum CoffeeSize
+    {
+        Medium,
+        Large
+    }
+}
---
+++ Interview/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Interview
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var order = new CoffeeOrder();
+            var shop = new CoffeeShop();
+            shop.CalculateCoffeeCost(order);
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
