26b8da040769b775de83ddc0f03f39f079b42bed
Toan Nguyen Van (SM) <toan.nguyen.van@stepmediasoftware.com>
2023-11-10 14:47:10 +0700

    Add LifetimeDi

---
+++ LifetimeDi/LifetimeDi/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LifetimeDi.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ LifetimeDi/LifetimeDi/SomeService.cs
@@ -0,0 +1,31 @@
+﻿namespace LifetimeDi
+{
+    public interface ITransientService
+    {
+        Guid GetID();
+    }
+
+    public interface IScopedService
+    {
+        Guid GetID();
+    }
+
+    public interface ISingletonService
+    {
+        Guid GetID();
+    }
+
+    public class SomeService : ITransientService, IScopedService, ISingletonService
+    {
+        Guid id;
+        public SomeService()
+        {
+            id = Guid.NewGuid();
+        }
+
+        public Guid GetID()
+        {
+            return id;
+        }
+    }
+}
